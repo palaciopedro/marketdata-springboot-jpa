990370d7864f5bb13cec720cad131c0fdad140ae
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-03-02 13:22:04 -0300

    project created

---
+++ salesdb/src/main/java/com/springproject/salesdb/SalesdbApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.salesdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesdbApplication.class, args);
+	}
+
+}
