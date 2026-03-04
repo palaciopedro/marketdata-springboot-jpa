7ac8c593b4599ba1971e5fba65df7eda4d6e19af
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-03-04 16:09:41 -0300

    many-to-many association between Product and Category

--- salesdb/src/main/java/com/springproject/salesdb/entities/Category.java
+++ salesdb/src/main/java/com/springproject/salesdb/entities/Category.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +22,7 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
