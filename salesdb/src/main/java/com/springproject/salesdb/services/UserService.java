bb244b184f3a39641c7aae2b9d976b679deccc5b
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-03-11 10:59:06 -0300

    update exception handling

--- salesdb/src/main/java/com/springproject/salesdb/services/UserService.java
+++ salesdb/src/main/java/com/springproject/salesdb/services/UserService.java
@@ -13,6 +13,8 @@ import com.springproject.salesdb.repositories.UserRepository;
 import com.springproject.salesdb.services.exceptions.DatabaseException;
 import com.springproject.salesdb.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
+		
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
